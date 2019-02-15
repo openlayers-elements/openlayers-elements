e6f8856c275fcff68a9b34c3222e2430e4347abf
tpluscode <tpluscode@users.noreply.github.com>
2019-02-15 14:14:02 +0100

    demo for reprojected layer

--- elements/swiss-topo-layer/swiss-topo-reprojected.ts
+++ elements/swiss-topo-layer/swiss-topo-reprojected.ts
@@ -4,20 +4,29 @@ import XYZ from 'ol/source/XYZ.js'
 import OlLayerBase from 'open-layers-elements/ol-layer-base'
 
 /**
- * A basic OpenStreetMap tile layer
+ * ### <swiss-topo-reprojected>
  *
- * @demo demo/swiss-topo.html
+ * A simpler form SwissTopo layers projects as Mercator.
+ *
+ * They will work nicely with standard layers such as OpenStreetMap and Bing
+ *
+ * #### Gotcha
+ *
+ * Apparently this style of layer source does not work with every one of Swiss layers. If you get 404s (blank map),
+ * use the [`swiss-topo-wmts` layer](#/elements/SwissTopoWMTS)
+ *
+ * @demo demo/swiss-reprojected.html
  * @customElement
  */
 @customElement('swiss-topo-reprojected')
 export class SwissTopoReprojected extends OlLayerBase<TileLayer> {
-    @property({ type: String, attribute: 'source-name' })
-    public sourceName: string
+    @property({ type: String, attribute: 'layer-name' })
+    public layerName: string
 
     public async createLayer() {
         return new TileLayer({
             source: new XYZ({
-                url: 'https://wmts10.geo.admin.ch/1.0.0/' + this.sourceName + '/default/current/3857/{z}/{x}/{y}.jpeg',
+                url: 'https://wmts10.geo.admin.ch/1.0.0/' + this.layerName + '/default/current/3857/{z}/{x}/{y}.jpeg',
             }),
         })
     }
