4ca0eecf24fff06896a76dbedb34d1514bc5a141
tpluscode <tpluscode@users.noreply.github.com>
2019-02-15 14:47:13 +0100

    updated docs for swiss wmts element

--- elements/swiss-topo-layer/swiss-topo-wmts.ts
+++ elements/swiss-topo-layer/swiss-topo-wmts.ts
@@ -9,28 +9,55 @@ import './projections'
 
 const parser = new WMTSCapabilities()
 
+type Projections = 'EPSG:3857' | 'EPSG:21718' | 'EPSG:2056' | 'EPSG:4329'
+
 /**
- * A basic OpenStreetMap tile layer
+ * ### <swiss-topo-wmts>
+ *
+ * Layer which loads official Swiss maps from [WMTS capabilities document][wmts-list]
+ *
+ * [wmts-list]: http://api3.geo.admin.ch/services/sdiservices.html#supported-projections
  *
  * @demo demo/swiss-topo.html
  * @customElement
  */
 @customElement('swiss-topo-wmts')
 export class SwissTopoWMTS extends OlLayerBase<TileLayer> {
-    @property({ type: String, attribute: 'source-name' })
-    public sourceName: string
+    /**
+     * One of the official layer names provided by geo.admin.ch.
+     *
+     * Complete list of layers is available
+     * [here](http://api3.geo.admin.ch/api/faq/index.html#which-layers-are-available)
+     *
+     * @type {string}
+     */
+    @property({ type: String, attribute: 'layer-name' })
+    public layerName: string
+
+    /**
+     * One of projection supported by swisstopo maps:
+     *
+     * 1. EPSG:3857 (Mercator) (default)
+     * 1. EPSG:2056
+     * 1. EPSG:21718
+     * 1. EPSG:4329
+     *
+     * @type {string}
+     */
+    @property( { type: String })
+    public projection: Projections = 'EPSG:3857'
 
     public async createLayer() {
-        const response = await fetch('https://wmts.geo.admin.ch/EPSG/3857/1.0.0/WMTSCapabilities.xml')
+        const projectionSegments = this.projection.replace(/:/, '/')
+        const response = await fetch(`https://wmts.geo.admin.ch/${projectionSegments}/1.0.0/WMTSCapabilities.xml`)
         const capabilities = parser.read(await response.text())
 
         const options = optionsFromCapabilities(capabilities, {
-            layer: this.sourceName,
-            matrixSet: 'EPSG:3857',
+            layer: this.layerName,
+            matrixSet: this.projection,
         })
 
         return new TileLayer({
-            opacity: 1,
             source: new WMTS(options),
         })
     }
