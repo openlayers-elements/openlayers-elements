11c4a48d44b1e20d3356560b935bb83a8543275a
tpluscode <tpluscode@users.noreply.github.com>
2019-02-21 19:58:01 +0100

    fix that test

--- elements/openlayers-elements/test/layer-xyz.test.js
+++ elements/openlayers-elements/test/layer-xyz.test.js
@@ -8,12 +8,12 @@ describe('ol-layer-xyz', () => {
   it('should pass url property to created layer', async () => {
     // given
     const url = 'http://layer.xyz/{x}/{y}/{z}.png'
-    const element = await fixture(html`<ol-layer-xyz url="${url}"></ol-layer-xyz>`) as OlLayerXyz
+    const element = await fixture(html`<ol-layer-xyz url="${url}"></ol-layer-xyz>`)
 
     // when
     const layer = await element.createPart()
 
     // then
-    expect(layer.getSource().getUrl()).to.be(url)
+    expect(layer.getSource().getUrls()).to.contain(url)
   })
 })
