77aecb7fead98d880173d5c32f5f4bdcde451af9
tpluscode <tpluscode@users.noreply.github.com>
2019-02-25 10:59:55 +0100

    further, make test code us typescript

--- elements/openlayers-elements/test/ol-layer-xyz.test.ts
+++ elements/openlayers-elements/test/ol-layer-xyz.test.ts
@@ -1,16 +1,17 @@
-/* global describe, it, beforeEach */
 import { expect, fixture } from '@open-wc/testing'
 import { html } from 'lit-html'
+import TileLayer from 'ol/layer/tile'
+import OlLayerBase from '../ol-layer-base'
 import '../ol-layer-xyz'
 
 describe('ol-layer-xyz', () => {
   it('should pass url property to created layer', async () => {
     // given
     const url = 'http://layer.xyz/{x}/{y}/{z}.png'
-    const element = await fixture(html`<ol-layer-xyz url="${url}"></ol-layer-xyz>`)
+    const element = await fixture(html`<ol-layer-xyz url="${url}"></ol-layer-xyz>`) as OlLayerBase<TileLayer>
 
     // when
-    const layer = await element.createPart()
+    const layer: any = await element.createPart()
 
     // then
     expect(layer.getSource().getUrls()).to.contain(url)
