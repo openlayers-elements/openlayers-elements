65dadb04e0136f9c34c28eb1c1a91ae40f9e6993
tpluscode <tpluscode@users.noreply.github.com>
2019-04-10 14:47:31 +0200

    spike for overlay element with auto-pan support

---
+++ elements/openlayers-elements/ol-overlay.ts
@@ -0,0 +1,59 @@
+import { OlMapPart } from '@openlayers-elements/core/ol-map-part';
+import Overlay from 'ol/overlay';
+import Map from 'ol/map';
+import { html } from 'lit-html';
+import { property } from 'lit-element';
+
+/**
+ * @demo https://openlayers-elements.netlify.com/demo/overlay.html
+ * @customElement
+ */
+export default class OlOverlay extends OlMapPart<Overlay> {
+  @property({ type: Boolean, attribute: 'auto-pan' })
+  autoPan: boolean
+
+  @property({ type: Number, attribute: 'auto-pan-animation-duration' })
+  autoPanAnimationDuration: number
+
+  overlay: Overlay
+
+  protected _addToMap(map: Map, overlay: Overlay) {
+    map.addOverlay(overlay)
+  }
+
+  protected _removeFromMap(map: Map, overlay: Overlay) {
+    map.removeOverlay(overlay)
+  }
+
+  async createPart() {
+    const slot = this.id
+    this.slot = slot
+
+    const slotEl = document.createElement('slot')
+    slotEl.name = slot
+
+    this.overlay = new Overlay({
+      element: slotEl,
+      autoPan: true,
+      autoPanAnimation: {
+        duration: this.autoPanAnimationDuration
+      } as any
+    })
+
+    // hack to get panning correctly calculate overlay dimensions,
+    // which would otherwise be calculated from the <slot> element
+    this.overlay.getElement = () => {
+      return this
+    }
+
+    return this.overlay
+  }
+
+  public render() {
+    return html`
+      <slot></slot>
+    `
+  }
+}
+
+customElements.define('ol-overlay', OlOverlay)
