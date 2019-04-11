4de0877b1407f1ec7e463d4c51792c27b1f154d6
tpluscode <tpluscode@users.noreply.github.com>
2019-04-11 08:50:51 +0200

    linting

--- elements/openlayers-elements/ol-overlay.ts
+++ elements/openlayers-elements/ol-overlay.ts
@@ -1,21 +1,21 @@
-import { OlMapPart } from '@openlayers-elements/core/ol-map-part';
-import Overlay from 'ol/Overlay';
-import Map from 'ol/map';
-import { html } from 'lit-html';
-import { property } from 'lit-element';
+import {OlMapPart} from '@openlayers-elements/core/ol-map-part'
+import Overlay from 'ol/Overlay'
+import Map from 'ol/map'
+import {html} from 'lit-html'
+import {property} from 'lit-element'
 
 /**
  * @demo https://openlayers-elements.netlify.com/demo/overlay.html
  * @customElement
  */
 export default class OlOverlay extends OlMapPart<Overlay> {
-  @property({ type: Boolean, attribute: 'auto-pan' })
-  autoPan: boolean
+  @property({type: Boolean, attribute: 'auto-pan'})
+  public autoPan: boolean
 
-  @property({ type: Number, attribute: 'auto-pan-animation-duration' })
-  autoPanAnimationDuration: number
+  @property({type: Number, attribute: 'auto-pan-animation-duration'})
+  public autoPanAnimationDuration: number
 
-  overlay: Overlay
+  protected _overlay: Overlay
 
   protected _addToMap(map: Map, overlay: Overlay) {
     map.addOverlay(overlay)
@@ -25,28 +25,28 @@ export default class OlOverlay extends OlMapPart<Overlay> {
     map.removeOverlay(overlay)
   }
 
-  async createPart() {
+  public async createPart() {
     const slot = this.id
     this.slot = slot
 
     const slotEl = document.createElement('slot')
     slotEl.name = slot
 
-    this.overlay = new Overlay({
+    this._overlay = new Overlay({
       element: slotEl,
       autoPan: true,
       autoPanAnimation: {
-        duration: this.autoPanAnimationDuration
-      } as any
+        duration: this.autoPanAnimationDuration,
+      } as any,
     })
 
     // hack to get panning correctly calculate overlay dimensions,
     // which would otherwise be calculated from the <slot> element
-    this.overlay.getElement = () => {
+    this._overlay.getElement = () => {
       return this
     }
 
-    return this.overlay
+    return this._overlay
   }
 
   public render() {
