dee71d6b3b203df2f8edece43f6da913c3197d8b
tpluscode <tpluscode@users.noreply.github.com>
2019-04-12 10:09:28 +0200

    incorrect import

--- karma.bs.config.js
+++ karma.bs.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies, @typescript-eslint/no-var-requires  */
 const merge = require('webpack-merge')
 const bsSettings = require('@open-wc/testing-karma-bs/bs-settings.js')
-const karmaEs5Config = require('./karma.es5.config.js')
+const karmaEs5Config = require('./karma.conf.js')
 
 module.exports = (config) => {
   config.set(
