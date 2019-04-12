f33e49ce7c8d2a103c77412b5ea68ec03f13d136
tpluscode <tpluscode@users.noreply.github.com>
2019-04-12 16:21:17 +0200

    update demo structure

---
+++ demos/demo/zoom-to-extent/index.js
@@ -0,0 +1,11 @@
+import loadPolyfills from '@open-wc/polyfills-loader'
+
+loadPolyfills()
+  .then(() => import('@openlayers-elements/maps/ol-map'))
+  .then(() => {
+    import('../../adaptive-demo')
+    import('@openlayers-elements/maps/ol-select')
+    import('@openlayers-elements/maps/ol-control')
+    import('@openlayers-elements/maps/ol-layer-geojson')
+    import('@openlayers-elements/maps/ol-layer-openstreetmap')
+  })
