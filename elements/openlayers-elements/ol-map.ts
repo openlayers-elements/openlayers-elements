b43478ac032f0da3ac310c14a4fba5a0df50eb6c
tpluscode <tpluscode@users.noreply.github.com>
2019-04-17 13:20:08 +0200

    typo

--- elements/openlayers-elements/ol-map.ts
+++ elements/openlayers-elements/ol-map.ts
@@ -1,4 +1,4 @@
 import OlMap from '@openlayers-elements/core/ol-map'
 
-console.warn('This inport is deprecated. Please use \'@openlayers-elements/core/ol-map\'')
+console.warn('This import is deprecated. Please use \'@openlayers-elements/core/ol-map\'')
 export default OlMap
