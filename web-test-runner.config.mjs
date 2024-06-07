8ecbc210d7dd5626c95b4cda67393128727f89dd
tpluscode <tpluscode@users.noreply.github.com>
2024-06-07 10:24:17 +0200

    ci: remove browserstack

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -2,54 +2,6 @@
 import { esbuildPlugin } from '@web/dev-server-esbuild'
 import { fromRollup } from '@web/dev-server-rollup'
 import commonjs from '@rollup/plugin-commonjs'
-import { browserstackLauncher } from '@web/test-runner-browserstack'
-
-let bsConfig = {}
-if (process.env.BROWSER_STACK_USERNAME && process.env.BROWSER_STACK_ACCESS_KEY) {
-  const sharedCapabilities = {
-    'browserstack.user': process.env.BROWSER_STACK_USERNAME,
-    'browserstack.key': process.env.BROWSER_STACK_ACCESS_KEY,
-
-    project: '@openlayers-elements/maps',
-    name: 'Tests',
-    build: `build ${process.env.GITHUB_RUN_NUMBER || 'unknown'}`,
-  }
-
-  bsConfig = {
-    concurrentBrowsers: 2,
-    browsers: [
-      // create a browser launcher per browser you want to test
-      // you can get the browser capabilities from the browserstack website
-      browserstackLauncher({
-        capabilities: {
-          ...sharedCapabilities,
-          browserName: 'Chrome',
-          os: 'Windows',
-          os_version: '10',
-        },
-      }),
-
-      browserstackLauncher({
-        capabilities: {
-          ...sharedCapabilities,
-          browserName: 'Safari',
-          browser_version: '14.1',
-          os: 'OS X',
-          os_version: 'Big Sur',
-        },
-      }),
-
-      browserstackLauncher({
-        capabilities: {
-          ...sharedCapabilities,
-          browserName: 'Edge',
-          os: 'Windows',
-          os_version: '10',
-        },
-      }),
-    ],
-  }
-}
 
 export default {
   files: 'elements/**/*.test.ts',
@@ -66,5 +18,4 @@ export default {
       ],
     }),
   ],
-  ...bsConfig,
 }
