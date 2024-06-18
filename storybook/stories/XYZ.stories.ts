b6542daed6efc6f4e9b3211febf4e344679f6ed4
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2024-06-18 14:54:04 +0200

    docs: update XYZ example

--- storybook/stories/XYZ.stories.ts
+++ storybook/stories/XYZ.stories.ts
@@ -16,15 +16,19 @@ const main: Meta = {
 export default main
 
 /**
- * For other layers you can check [OSM wiki](https://wiki.openstreetmap.org/wiki/Tile_servers).
+ * For other layers you can check [OSM wiki](https://wiki.openstreetmap.org/wiki/Raster_tile_providers).
  *
  * (Note the different style of interpolating x/y/z parameters)
  */
 export const Basic: Story = {
-  name: 'German OpenStreetmap XYZ layer overlayed with cycling paths',
-  render: () => html`
-<ol-map zoom="7" lat="46.7985" lon="8.2318">
-  <ol-layer-xyz url="https://a.tile.openstreetmap.de/{z}/{x}/{y}.png "></ol-layer-xyz>
-  <ol-layer-xyz url="https://tile.waymarkedtrails.org/cycling/{z}/{x}/{y}.png"></ol-layer-xyz>
+  name: 'Stadia Map overlayed with horse riding routes',
+  args: {
+    layer1: 'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}.png',
+    layer2: 'https://tile.waymarkedtrails.org/riding/{z}/{x}/{y}.png',
+  },
+  render: ({ layer1, layer2 }) => html`
+<ol-map zoom="6" lat="46.7985" lon="8.2318">
+  <ol-layer-xyz url="${layer1}"></ol-layer-xyz>
+  <ol-layer-xyz url="${layer2}"></ol-layer-xyz>
 </ol-map>`,
 }
