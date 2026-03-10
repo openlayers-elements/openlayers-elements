4957b15fb71d73f2f4b23ad7168c9d5b396d465d
tpluscode <tpluscode@users.noreply.github.com>
2026-03-10 14:29:52 +0100

    test: remove failing case which cannot be easily reproduced

--- elements/openlayers-core/test/ol-layer-vector.test.ts
+++ elements/openlayers-core/test/ol-layer-vector.test.ts
@@ -1,7 +1,5 @@
-import { expect, fixture, assert, aTimeout } from '@open-wc/testing'
+import { expect, fixture, aTimeout } from '@open-wc/testing'
 import { html } from 'lit'
-import * as sinon from 'sinon'
-import type OlMap from '../ol-map.js'
 import type OlLayerVector from '../ol-layer-vector.js'
 import '../ol-layer-vector.js'
 import '../ol-map.js'
@@ -64,28 +62,5 @@ describe('ol-layer-vector', () => {
 
     // then
     expect(element.source!.getFeatures().length).to.equal(1)
-  })
-
-  describe('fit', () => {
-    it('calls fit on the underlying map', async () => {
-      // given
-      const map = (await fixture(
-        html`
-        <ol-map>
-          <ol-layer-vector></ol-layer-vector>
-        </ol-map>
-      `,
-      )) as OlMap
-      const layer = map.querySelector('ol-layer-vector') as OlLayerVector
-
-      const mapFit = sinon.spy(map, 'fit')
-      sinon.stub(layer.source!, 'getExtent').callsFake(() => [1, 2, 3, 4])
-
-      // when
-      layer.fit()
-
-      // then
-      assert(mapFit.calledWith([1, 2, 3, 4]))
-    })
   })
 })
